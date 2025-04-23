a8b65928df1761810ccf87ebbce01f15349da89a
ArturLRA <artur.luiz.rodrigues.alves@gmail.com>
2025-04-22 22:12:19 -0300

    fourth commit

--- Index.js
+++ Index.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', () => {
     }
 });
 
-const filters = ['subjectFilter', 'trimesterFilter', 'toolFilter', 'languageFilter'];
+const filters = ['subjectFilter', 'trimesterFilter', 'toolFilter'];
 
 
 const projectCards = document.querySelectorAll('.projectCard');
@@ -20,52 +20,32 @@ filters.forEach(id => {
 
 
 function filterProjects() {
-
     const subject = document.getElementById('subjectFilter').value;
     const trimester = document.getElementById('trimesterFilter').value;
     const tool = document.getElementById('toolFilter').value;
-    const language = document.getElementById('languageFilter').value;
-
 
     projectCards.forEach(card => {
-
         const matchSubject = subject === '' || card.dataset.subject === subject;
         const matchTrimester = trimester === '' || card.dataset.trimester === trimester;
         const matchTool = tool === '' || card.dataset.tool === tool;
-        const matchLanguage = language === '' || card.dataset.language === language;
 
-
-        if (matchSubject && matchTrimester && matchTool && matchLanguage) {
+        if (matchSubject && matchTrimester && matchTool) {
             card.style.display = 'block';
         } else {
             card.style.display = 'none';
         }
     });
-}
 
-projectCards.forEach(card => {
-    card.addEventListener('click', () => {
-      const modal = document.getElementById('projectModal');
-      document.getElementById('modalImg').src = card.querySelector('img').src;
-      document.getElementById('modalTitle').textContent = card.querySelector('h3').textContent;
-      document.getElementById('modalDescription').textContent = card.querySelector('p').textContent;
-      if (card.dataset.link === '') {
-        document.getElementById('modalLink').style.display = 'none';
-      }
-      else {
-        document.getElementById('modalLink').href = card.dataset.link;
-      }
 
-      modal.style.display = 'flex';
+    const blocks = document.querySelectorAll('.mathBlocks, .portugueseBlocks, .scienceBlocks, .historyBlocks, .tiBlocks');
+    blocks.forEach(block => {
+        const visibleCards = block.querySelectorAll('.projectCard[style*="display: block"]');
+        const title = block.querySelector('h1');
+
+        if (visibleCards.length === 0) {
+            title.style.display = 'none';
+        } else {
+            title.style.display = 'block';
+        }
     });
-  });
-  
-  document.querySelector('.closeBtn').addEventListener('click', () => {
-    document.getElementById('projectModal').style.display = 'none';
-  });
-  
-  window.addEventListener('click', (e) => {
-    if (e.target.classList.contains('modal')) {
-      e.target.style.display = 'none';
-    }
-  });
+}
